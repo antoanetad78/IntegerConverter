0d4bfa248c4a59549ce0f0f8b8d6711357c91aad
antoanetad78 <antoanetad78@gmail.com>
2019-07-02 20:49:31 +0200

    gitignore added and minor cleaning up and name changes

--- NumberSpellingConverter.js
+++ NumberSpellingConverter.js
@@ -46,77 +46,76 @@ const ranges = {
 //A functions that takes an array, divides it into three-element arrays and returns an array of the new arrays
 //Example input: [ '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 //Example output: [ [ '1', '2', '3' ], [ '4', '5', '6' ], [ '7', '8', '9' ] ]
-const div = arr => {
+const separate = arr => {
   const newArr = [];
   const arr1 = arr;
-  const divider = arr => {
+  const separator = arr => {
     if (arr.length <= 3) {
       return newArr.unshift(arr);
     }
     const tempArr = arr.splice(-3, 3);
     newArr.unshift(tempArr);
-    divider(arr);
+    separator(arr);
   };
-  divider(arr1);
+  separator(arr1);
   return newArr;
 };
 
-// Convert an array of three elements, all numbers from 0-9 - into the corresponding words from the dictionaries
 const baseConvert = arr => {
-  const hundr = arr[0];
-  const tn = arr[1];
-  const ons = arr[2];
+  const hundred = arr[0];
+  const ten = arr[1];
+  const one = arr[2];
 
   let converted = [];
-  if (hundr === 0 && tn === 0 && ons === 0) {
-    // return (converted = converted.concat(zero));
-  }
-  if (hundr === 0) {
-    if (tn === 0) {
-      return (converted = converted.concat(ones[ons]));
+  // if (hundred === 0 && ten === 0 && one === 0) {
+  //   // return (converted = converted.concat(zero));
+  // }
+  if (hundred === 0) {
+    if (ten === 0) {
+      return (converted = converted.concat(ones[one]));
     } else {
-      if (tn === 1 && ons > 0) {
-        return (converted = converted.concat(teens[ons]));
+      if (ten === 1 && one > 0) {
+        return (converted = converted.concat(teens[one]));
       }
-      if (tn === 1 && ons === 0) {
-        return (converted = converted.concat(tens[tn]));
+      if (ten === 1 && one === 0) {
+        return (converted = converted.concat(ten[ten]));
       }
-      if (tn > 1 && ons === 0) {
-        return (converted = converted.concat(tens[tn]));
+      if (ten > 1 && one === 0) {
+        return (converted = converted.concat(ten[ten]));
       }
-      if (tn > 1 && ons > 0) {
-        return (converted = converted.concat(tens[tn] + " " + ones[ons]));
+      if (ten > 1 && one > 0) {
+        return (converted = converted.concat(ten[ten] + " " + ones[one]));
       }
     }
   }
-  if (hundr > 0 && tn === 0) {
-    if (ons === 0) {
-      return converted.concat(ones[hundr] + " hundred");
+  if (hundred > 0 && ten === 0) {
+    if (one === 0) {
+      return converted.concat(ones[hundred] + " hundred");
     } else {
       return (converted = converted.concat(
-        ones[hundr] + " hundred and " + ones[ons]
+        ones[hundred] + " hundred and " + ones[one]
       ));
     }
   }
-  if (hundr > 0 && tn === 1) {
-    if (ons === 0) {
+  if (hundred > 0 && ten === 1) {
+    if (one === 0) {
       return (converted = converted.concat(
-        ones[hundr] + " hundred and " + tens[tn]
+        ones[hundred] + " hundred and " + ten[ten]
       ));
     } else {
       return (converted = converted.concat(
-        ones[hundr] + " hundred and " + teens[ons]
+        ones[hundred] + " hundred and " + teens[one]
       ));
     }
   }
-  if (hundr > 0 && tn > 1) {
-    if (ons === 0) {
+  if (hundred > 0 && ten > 1) {
+    if (one === 0) {
       return (converted = converted.concat(
-        ones[hundr] + " hundred and " + tens[tn]
+        ones[hundred] + " hundred and " + ten[ten]
       ));
     } else {
       return (converted = converted.concat(
-        ones[hundr] + " hundred and " + tens[tn] + " " + ones[ons]
+        ones[hundred] + " hundred and " + ten[ten] + " " + ones[one]
       ));
     }
   }
@@ -130,7 +129,7 @@ const isZero = arr =>
     return acc;
   }, true);
 //Takes an array and fills it up with leading zeros untill the lenght of the array is 3
-const fillUpArr = arr => {
+const fillUpArrayWithLeadingZeros = arr => {
   for (let i = arr.length; i < 3; i++) {
     arr.unshift(0);
   }
@@ -138,7 +137,7 @@ const fillUpArr = arr => {
 };
 
 //Converts an array of numerical strings to an array of numbers
-const numSringToNumber = arr => {
+const numericalSringArrayToNumberArray = arr => {
   return arr.map(el => Number(el));
 };
 
@@ -149,66 +148,55 @@ const convertNumber = num => {
         " Please provide a valid number. Valid is a number of up to 15 numerical characters"
       );
     }
-
-    // If it's a number, convert it to string
     const numToString = num.toString(10);
 
-    //CHeck if the length is within the range - 15 digits long
     if (numToString.length > 15) {
       throw new Error(
         " The number is too large. Please use a number of up to 15 digits."
       );
     } else {
-      const numToString = num.toString(10); //convert the number to string to be able to spilt
-      const numToStringArr = numToString.split(""); // spilt to individual numbers, in string form
-      if (numToStringArr.length < 3) {
-        fillUpArr(numToStringArr);
+      const numToString = num.toString(10);
+      const numToStringArray = numToString.split("");
+      if (numToStringArray.length < 3) {
+        fillUpArrayWithLeadingZeros(numToStringArray);
       }
-      const numArray = numSringToNumber(numToStringArr); // convert each individual numerical string to number. result is an array of numbers
+      const numbersArray = numericalSringArrayToNumberArray(numToStringArray);
 
-      let arrNumber = div(numArray); // divide the array of numbers into portions of threee numbers each. result is an array like this: [ [1,2,3], [4,5,6], [7,8,9] ]
-      // console.log(arrNumber, " Original number array");
+      let numberSplittedIntoRangesArray = separate(numbersArray);
 
-      // Check if the array of number arrays has only one element (e.g. - if we only have one three digit number)
-      // Returns zero or the passed number of up to three digits
-      if (arrNumber.length === 1) {
-        arrNumber = arrNumber.flat(); // Flatten the array for easier use
-        const probe = isZero(arrNumber);
-        console.log(probe);
-
+      if (numberSplittedIntoRangesArray.length === 1) {
+        numberSplittedIntoRangesArray = numberSplittedIntoRangesArray.flat();
         let converted;
-        probe
+        isZero(numberSplittedIntoRangesArray)
           ? (converted = zero)
-          : (converted = baseConvert(arrNumber).join(" "));
+          : (converted = baseConvert(numberSplittedIntoRangesArray).join(" "));
         console.log(converted);
         return converted;
       }
-      // This code runs if the array of numerical arrays has more than one element ( e.g. - we have passed a number > 999)
-      arrNumber.reverse(); // reverse the positions of the numerical arrays
+      numberSplittedIntoRangesArray.reverse();
 
-      // convert the reversed array
-      let converted = arrNumber.reduce((acc, curr, index) => {
-        if (curr.length < 3) {
-          fillUpArr(curr);
-        }
-        if (index === 0) {
-          if (curr[0] === 0) {
-            if (isZero(curr)) {
+      let converted = numberSplittedIntoRangesArray.reduce(
+        (acc, curr, index) => {
+          if (curr.length < 3) {
+            fillUpArrayWithLeadingZeros(curr);
+          }
+          if (index === 0) {
+            if (curr[0] === 0) {
+              isZero(curr) ? acc : acc.unshift("and " + baseConvert(curr));
               return acc;
             }
-            acc.unshift("and " + baseConvert(curr));
+            acc.unshift(baseConvert(curr));
             return acc;
           }
-          acc.unshift(baseConvert(curr));
+          if (isZero(curr)) {
+            return acc;
+          }
+          acc.unshift(baseConvert(curr) + ranges[index]);
+          acc = acc.flat();
           return acc;
-        }
-        if (isZero(curr)) {
-          return acc;
-        }
-        acc.unshift(baseConvert(curr) + ranges[index]);
-        acc = acc.flat();
-        return acc;
-      }, []);
+        },
+        []
+      );
       converted = converted.join(" ");
       console.log(converted);
       return converted;
@@ -220,4 +208,4 @@ const convertNumber = num => {
 };
 
 module.exports = convertNumber;
-// convertNumber(084);
+convertNumber(2000502);
